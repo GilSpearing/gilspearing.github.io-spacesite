5c8bafd23420b9bfe7a4e17aa6becf89897c6bba
Gil Spearing <gilspearing@gils-mbp.home>
2020-06-19 20:44:18 +0100

    Updates

---
+++ precache-manifest.d323c5d0bed2520070a2efc2135dcfb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a11b7e7b77a668078eef7a1b8a5122d",
+    "url": "/spacesite/index.html"
+  },
+  {
+    "revision": "e59a17a912cd3ea9b1eb",
+    "url": "/spacesite/static/css/main.52448527.chunk.css"
+  },
+  {
+    "revision": "ab9dc9f7ee80e0a5f6df",
+    "url": "/spacesite/static/js/2.4fc47af3.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/spacesite/static/js/2.4fc47af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59a17a912cd3ea9b1eb",
+    "url": "/spacesite/static/js/main.781fecfb.chunk.js"
+  },
+  {
+    "revision": "e843207d50594eb8d0cf",
+    "url": "/spacesite/static/js/runtime-main.0abd3046.js"
+  }
+]);
